0fe786f8c63cbba60628e92f916843083ea9b2f7
James Tirta Halim <tirtajames45@gmail.com>
2026-01-31 02:52:50 +0700

    examples: fix

--- temp.h
+++ temp.h
@@ -5,11 +5,11 @@
 
 static const char *c_table_temps[] = {
 	/* For example:
-	"/sys/class/hwmon/hwmon2/temp2_name",
-	"/sys/class/hwmon/hwmon2/temp3_name",
-	"/sys/class/hwmon/hwmon2/temp4_name",
-	"/sys/class/hwmon/hwmon2/temp5_name",
-	"/sys/class/hwmon/hwmon2/temp6_name",
-	"/sys/class/hwmon/hwmon2/temp7_name", */
+	"/sys/class/hwmon/hwmon2/temp2_input",
+	"/sys/class/hwmon/hwmon2/temp3_input",
+	"/sys/class/hwmon/hwmon2/temp4_input",
+	"/sys/class/hwmon/hwmon2/temp5_input",
+	"/sys/class/hwmon/hwmon2/temp6_input",
+	"/sys/class/hwmon/hwmon2/temp7_input", */
 	CPU_TEMP_FILE
 };
