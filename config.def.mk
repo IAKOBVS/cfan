ea024a5496927a786ab1c3f6d8f5cc060d6d2d9e
James Tirta Halim <tirtajames45@gmail.com>
2026-02-11 22:24:58 +0700

    config.mk: disable nvidia by default

--- config.def.mk
+++ config.def.mk
@@ -1,4 +1,4 @@
 # NVML (uncomment to disable)
 LIB_CUDA = /opt/cuda/lib64
-LDFLAGS_CUDA = -L$(LIB_CUDA) -lnvidia-ml
-REQ_CUDA = gpu-nvidia.h
+# LDFLAGS_CUDA = -L$(LIB_CUDA) -lnvidia-ml
+# REQ_CUDA = gpu-nvidia.h
